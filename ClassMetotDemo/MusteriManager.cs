8e5d1809d5de576beaad95db7f9571c88520025e
Muhammed fatih Çelik <mamibesiktas@outlook.com>
2021-01-17 17:45:10 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Show(Musteri musteri)
+        {
+                Console.WriteLine("Id : " + musteri.Id);
+                Console.WriteLine("Ad : " + musteri.Ad);
+                Console.WriteLine("Soyad : " + musteri.Soyad);
+                Console.WriteLine("Bakiye : " + musteri.Bakiye);
+                Console.WriteLine("********************");
+        }
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi, Merhaba " + musteri.Ad);
+        }
+
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi, " + musteri.Id);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi, " + musteri.Id);
+        }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Muhammed Fatih";
+            musteri1.Soyad = "Çelik";
+            musteri1.Bakiye = 5000;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Engin";
+            musteri2.Soyad = "Demiroğ";
+            musteri2.Bakiye = 8000;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Nejla";
+            musteri3.Soyad = "Çelik";
+            musteri3.Bakiye = 4750;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Id = 4;
+            musteri4.Ad = "Erkan";
+            musteri4.Soyad = "Çelik";
+            musteri4.Bakiye = 6000;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Show(musteri1);
+            musteriManager.Show(musteri2);
+            musteriManager.Show(musteri3);
+            musteriManager.Show(musteri4);
+
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+            musteriManager.Add(musteri4);
+
+            musteriManager.Update(musteri1);
+            musteriManager.Update(musteri2);
+            musteriManager.Update(musteri3);
+            musteriManager.Update(musteri4);
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Delete(musteri3);
+            musteriManager.Delete(musteri4);
+        }
+    }
+}
